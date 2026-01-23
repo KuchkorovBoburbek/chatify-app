979407fa9206f7da3928671c297ae1c65ae5feb6
Boburbek Kuchqorov <bekkr0321@gmail.com>
2026-01-24 01:52:01 +0900

    engines field added in package.json

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,17 @@
-import express from "express"
-import { signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
-import {protectRoute} from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import arcjet from "@arcjet/node";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
-const router = express.Router()
+const router = express.Router();
 
 router.use(arcjetProtection);
-// bu yerda kelayotgan hamma reqlar arcjetProtection tekshiruvidaan otadi va uni ichidagi next() ishlaydi yani keyingi qatorga otqazadi 
+// bu yerda kelayotgan hamma reqlar arcjetProtection tekshiruvidaan otadi va uni ichidagi next() ishlaydi yani keyingi qatorga otqazadi
 // SignUp, Login LogOut section
 //   SignUp
 router.post("/signup", signup);
@@ -15,7 +20,7 @@ router.post("/login", login);
 //   LogOut
 router.post("/logout", logout);
 // update profile logic
-router.put("/update-profile", protectRoute,  updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 // router.get("/check", protectRoute, (req, res)=>res.status(200).json(req.user))
 
 export default router;

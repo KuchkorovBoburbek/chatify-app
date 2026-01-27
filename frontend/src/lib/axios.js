cd94fc4a41462042fbd0423b64dad1f288d2a236
Boburbek Kuchqorov <bekkr0321@gmail.com>
2026-01-27 16:05:30 +0900

    Frontend setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5173/api" //axiosInstance.get("/users") => http://localhost:5173/api/users
+      : "/api",
+  withCredentials: true, //cookie’larni so‘rov bilan birga yuborish
+});

44db71a0eb1a5cd1a02d8adf23101eb5a1613a72
Boburbek Kuchqorov <bekkr0321@gmail.com>
2026-01-28 03:16:00 +0900

    signup implemented on frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5173/api" //axiosInstance.get("/users") => http://localhost:5173/api/users
+      ? "http://localhost:5003/api" //axiosInstance.get("/users") => http://localhost:5003/api/users
       : "/api",
   withCredentials: true, //cookie’larni so‘rov bilan birga yuborish
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,39 @@
 
-import { create } from "zustand";
-
+import { create } from "zustand"; //Zustand — React uchun yengil state manager
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
-  authUser: { name: "Mark", _id: 123, age: 25 },
-  isLoggedIn: false,
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
 
-  login: () => {
-    set({isLoggedIn: true})
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      if (typeof res.data !== "object") {
+        throw new Error("Invalid auth response");
+      }
+
+      set({ authUser: res.data });
+    } catch (err) {
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
   },
+
+  signup: async (data)=>{
+    set({ isSigningUp : true});
+     try {
+      const res = await axiosInstance.post("auth/signup", data)
+      set({authUser: res.data});
+
+      toast.success("Account created successfully 🥳🥳🥳");
+     } catch (error) {
+      toast.error(error.response.data.message)
+     } finally{
+    set({ isSigningUp: false });
+     }
+  }
 }));
